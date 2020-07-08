7ed11653f57a862ad5f69ab21eaa26be972ca54c
root2raj <ruturaj1999@gmail.com>
2020-07-09 02:38:38 +0530

    vince: Nuke Snap
    * Also nuke Snap overlays

--- device.mk
+++ device.mk
@@ -136,8 +136,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
-    libmm-qcamera \
-    Snap
+    libmm-qcamera
 
 # Consumerir
 PRODUCT_PACKAGES += \
