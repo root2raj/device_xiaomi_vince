579ec508094048175c5b07f0bf35e13ccbc87968
xyyx <xyyx@mail.ru>
2020-07-28 13:27:51 +0530

    vince: Add basic USB HAL that reports no status change

--- device.mk
+++ device.mk
@@ -384,7 +384,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.vince
 
 # Vibrator
 PRODUCT_PACKAGES += \
