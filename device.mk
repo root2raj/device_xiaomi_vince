dfec24e6a8d41db5663b74385c2a5adddb85cf5a
GiaSen <giammasena@gmail.com>
2020-09-22 20:44:52 +0530

    vince: Copy vndk-29 libbinderthreadstate.so to vendor
    09-11 14:55:24.310  1517  1517 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/gx_fpd": library "libbinderthreadstate.so" not found: needed by /vendor/lib64/libbindergx.so in namespace (default)

--- device.mk
+++ device.mk
@@ -432,6 +432,9 @@ PRODUCT_PACKAGES += \
     vndk_package \
     libstdc++.vendor
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libbinderthreadstate.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbinderthreadstate.so
+
 # VR
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
