ce699c89ae75d104dd056b62f01c6699b560e295
root2raj <ruturaj1999@gmail.com>
2020-10-16 05:40:44 +0000

    Revert "vince: Add basic USB HAL that reports no status change"
    This reverts commit 579ec508094048175c5b07f0bf35e13ccbc87968.

--- device.mk
+++ device.mk
@@ -422,7 +422,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.vince
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
