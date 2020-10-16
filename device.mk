ba0124bd90d561257163e1a926fd01aedc3f0eca
Lucchetto <lucchetto.tie@live.com>
2020-10-16 09:48:46 +0530

    vince: Build libhidltransport * Required by some blobs

--- device.mk
+++ device.mk
@@ -233,7 +233,9 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0-java \
+    libhidltransport \
+    libhidltransport.vendor
 
 # HW crypto
 PRODUCT_PACKAGES += \
