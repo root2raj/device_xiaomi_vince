fc877b62e3e8a0fae534aa84a05eb5bea74d1185
root2raj <ruturaj1999@gmail.com>
2021-04-20 20:00:37 +0000

    vince: Switch to redfin april fp

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -39,4 +39,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys"
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
